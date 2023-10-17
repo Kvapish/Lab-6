7b5eb4de70cee881e5e14dfe8efb8819c2339b44
Kvapish Alexander <kvapis@gmail.com>
2023-10-17 22:56:33 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -11,33 +11,27 @@ class Program
     {
         List<IPerson> people = new List<IPerson>();
 
-        // Добавление объектов в массив
         people.Add(new Employee("1", "Александр", 30));
         people.Add(new Employee("2", "Дмитрий", 25));
 
-        // Вывод содержимого массива
         Console.WriteLine("Люди в массиве:");
         foreach (var person in people)
         {
             Console.WriteLine(person.ToString());
         }
 
-        // Редактирование элемента массива
         Employee employeeToUpdate = (Employee)people[0];
         employeeToUpdate.Name = "Обновленный Александр";
         employeeToUpdate.Age = 31;
 
-        // Вывод обновленного содержимого массива
         Console.WriteLine("\nОбновленные люди в массиве:");
         foreach (var person in people)
         {
             Console.WriteLine(person.ToString());
         }
 
-        // Удаление элемента из массива
         people.RemoveAt(1);
 
-        // Вывод обновленного содержимого массива после удаления
         Console.WriteLine("\nЛюди в массиве после удаления:");
         foreach (var person in people)
         {
